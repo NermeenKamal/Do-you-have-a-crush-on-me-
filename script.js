c85b0684a27c24a33de3eae060ef11ef2e90a585
Nermeen Kamal <114883845+NermeenKamal@users.noreply.github.com>
2024-09-06 17:48:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-
-
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 
-// Change text and gif when the Yes button is clicked
-yesBtn.addEventListener("click", () => {
+// Change text and gif when the Yes button is clicked or touched
+const onYesClick = () => {
     question.innerHTML = "ME TOOOOO! 😘";
     gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOTJ0NDZ4enBiNzQ4bXFidnA3dTZ5cGQxaWR3aW9sZng4N3I3Z2dheCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MSBEiH9mCp39K/giphy.gif";
-});
+};
 
-// Make the No button move randomly on hover
-noBtn.addEventListener("mouseover", () => {
+// Handle touch and mouse click for the Yes button
+yesBtn.addEventListener("click", onYesClick);
+yesBtn.addEventListener("touchstart", onYesClick);
+
+// Function to make No button move randomly
+const moveNoBtn = () => {
     const wrapper = document.querySelector(".wrapper");
     const wrapperRect = wrapper.getBoundingClientRect();
     const noBtnRect = noBtn.getBoundingClientRect();
@@ -26,9 +28,8 @@ noBtn.addEventListener("mouseover", () => {
 
     noBtn.style.left = randomX + "px";
     noBtn.style.top = randomY + "px";
-});
+};
 
-
-
-
-
+// Make No button move randomly on hover or touch
+noBtn.addEventListener("mouseover", moveNoBtn);
+noBtn.addEventListener("touchstart", moveNoBtn);
